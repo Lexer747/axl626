cce6bc0b920d1a3836e76fe8ad1b5c671b2ef048
Alexander Lewis <axl626@cs.bham.ac.uk>
2019-01-24 11:51:32 +0000

    added better way to compare dates

--- Application/Portfolio/src/Risk.hs
+++ Application/Portfolio/src/Risk.hs
@@ -11,3 +11,12 @@ getWindow 0     end (Just x:xs) = [x] ++ (getWindow 0 (end - 1) xs)
 getWindow 0     end (Nothing:xs) = (getWindow 0 (end - 1) xs)
 getWindow start end (_:xs) = getWindow (start - 1) (end - 1) xs
 
+calcMean :: [Double] -> Double
+calcMean xs = (sum xs) / (fromIntegral $ length xs)
+
+calcVariance :: [Double] -> Double -> Double
+calcVariance xs mean = let inner = map (\x -> (x - mean) ** 2) xs in
+                       (sum inner) / (fromIntegral $ length xs)
+
+getSameMonthAndYear :: String -> String -> Bool
+getSameMonthAndYear 
--- Application/Portfolio/src/SemiDate.hs
+++ Application/Portfolio/src/SemiDate.hs
@@ -3,6 +3,7 @@ module SemiDate where
 -- "YYYY-MM-DD"
 
 type CompareFunc = Integer -> Integer -> Bool
+data Date = Year | Month | Day
 
 compareYear :: CompareFunc -> String -> String -> Bool
 compareYear f (x1:x2:x3:x4:_) (y1:y2:y3:y4:_) = f x y
@@ -27,6 +28,26 @@ almostEq :: (Eq a, Enum a) => Integer -> a -> a -> Bool
 almostEq 0 x y             = x == y
 almostEq _ x y | x == y    = True
 almostEq n x y | otherwise = almostEq (n - 1) (succ x) y
+
+findBasedOnDate :: [(CompareFunc, Date)] -> (Bool -> Bool -> Bool) -> String -> String -> Bool
+findBasedOnDate fs combine x y = my_fold combine compared
+    where compared = map (\(f,d) -> applyBasedOnDate f d x y) fs
+
+my_fold :: (a -> a -> a) -> [a] -> a
+my_fold _ []     = error "fold called with empty list"
+my_fold _ (x:[]) = x
+my_fold f (x:x':xs) = my_fold_help (x':xs) f (f x x')
+
+my_fold_help :: [a] -> (a -> a -> a) -> a -> a
+my_fold_help []        _ _   = error "fold called with 0 args"
+my_fold_help (x:[])    f acc = f x acc
+my_fold_help (x:xs) f acc = my_fold_help xs f (f x acc)
+
+
+applyBasedOnDate :: CompareFunc -> Date -> String -> String -> Bool
+applyBasedOnDate f Year  = compareYear f
+applyBasedOnDate f Month = compareMonth f
+applyBasedOnDate f Day   = compareDay f
 
 --sort dates:
 --if f x y then false 
